e1f11d3d649763c713736a420d3362a0706cd7e4
G00Z_G00Z <edygomezgg4@gmail.com>
2022-04-18 18:12:30 -0500

    Made simulation in Proteus work

--- estacionamiento.cpp
+++ estacionamiento.cpp
@@ -99,8 +99,18 @@ public:
 
 void setup()
 {
+
+    Serial.begin(9600);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
+    Serial.println("Hola como estas!!");
+    // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000); // Wait for 1000 millisecond(s)
+    // turn the LED off by making the voltage LOW
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000); // Wait for 1000 millisecond(s)
 }
