2e9fcf5d6d5ee3f9196186a0f42f4e268bf2b136
G00Z_G00Z <edygomezgg4@gmail.com>
2022-04-18 11:46:49 -0500

    Added a counter with limit class

--- estacionamiento.ino
+++ estacionamiento.ino
@@ -1,9 +1,55 @@
+
+/*
+ * CounterwithLimit
+ *
+ * It keeps track of a count, in which it has a lower bound and an upper bound
+ * [l,u). It also shows if you are on a limit.
+ */
+class CounterWithLimit
+{
+
+private:
+    int lowerBound;
+    int upperBound;
+    int count = 0;
+
+public:
+    CounterWithLimit(int lowerBound, int upperBound, int initialValue)
+    {
+        this->lowerBound = lowerBound;
+        this->upperBound = upperBound;
+        this->count = initialValue;
+    }
+
+public:
+    bool isOnLimit()
+    {
+        return this->count == this->upperBound || this->count == this->lowerBound;
+    }
+
+    int increment()
+    {
+        if (this->upperBound > 1 + this->count)
+        {
+            this->count++;
+        }
+        return this->count;
+    }
+
+    int decrement()
+    {
+        if (this->lowerBound <= 1 + this->count)
+        {
+            this->count--;
+        }
+        return this->count;
+    }
+};
+
 void setup()
 {
-	
 }
 
 void loop()
 {
-	
 }
